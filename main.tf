0246faeed813daaeecb0b2df240c776bf4b45011
varmanuk <varmanuk@gmail.com>
2026-05-01 19:08:20 +0530

    remove resources

--- main.tf
+++ main.tf
@@ -27,35 +27,35 @@ resource "random_id" "suffix" {
   byte_length = 4
 }
 
-# EC2 Instance
-resource "aws_instance" "example" {
-  ami           = data.aws_ami.amazon_linux.id
-  instance_type = "t2.micro"
+# # EC2 Instance
+# resource "aws_instance" "example" {
+#   ami           = data.aws_ami.amazon_linux.id
+#   instance_type = "t2.micro"
 
-  tags = {
-    Name    = "spacelift-ec2"
-    env     = var.env
-    owner   = var.owner
-    testtag = "true"
-  }
-}
+#   tags = {
+#     Name    = "spacelift-ec2"
+#     env     = var.env
+#     owner   = var.owner
+#     testtag = "true"
+#   }
+# }
 
-# S3 Bucket
-resource "aws_s3_bucket" "bucket" {
-  bucket = "spacelift-demo-${var.env}-${random_id.suffix.hex}"
+# # S3 Bucket
+# resource "aws_s3_bucket" "bucket" {
+#   bucket = "spacelift-demo-${var.env}-${random_id.suffix.hex}"
 
-  tags = {
-    env   = var.env
-    owner = var.owner
-  }
-}
+#   tags = {
+#     env   = var.env
+#     owner = var.owner
+#   }
+# }
 
-# Block Public Access
-resource "aws_s3_bucket_public_access_block" "block" {
-  bucket = aws_s3_bucket.bucket.id
+# # Block Public Access
+# resource "aws_s3_bucket_public_access_block" "block" {
+#   bucket = aws_s3_bucket.bucket.id
 
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
-}
+#   block_public_acls       = true
+#   block_public_policy     = true
+#   ignore_public_acls      = true
+#   restrict_public_buckets = true
+# }
